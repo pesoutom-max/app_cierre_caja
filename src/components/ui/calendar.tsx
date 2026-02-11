0f7346332f92396c4b3b411017e70edc5a644f48
Pedro Souto <soutopedro5@gmail.com>
2026-02-11 21:44:45 +0000

    Muéstrame el código completo actual de src/components/ui/calendar.tsx y

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -56,9 +56,9 @@ function Calendar({
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
-      {...props}
       locale={es}
       weekStartsOn={1}
+      {...props}
     />
   )
 }
